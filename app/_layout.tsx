4cb85e74a410028a51c21eac2d6426b5e7238de7
SoundtrkSamurai <furtimat3@yahoo.com>
2025-10-06 21:09:51 -0700

    Add Themed component and update layout to use it

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,14 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 export default function RootLayout() {
   return (
     <SafeAreaProvider>
-      <Stack />
+      <Stack
+        screenOptions={{
+          headerTintColor: "#fff",
+          headerStyle: { backgroundColor: "#000" },
+        }}
+      >
+        <Stack.Screen name="index" options={{ title: "Home" }} />
+      </Stack>
     </SafeAreaProvider>
   );
 }
